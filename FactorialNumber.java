d2f8e40bba3253c59dd33ce6f1521d44836ad6ee
NANDAN S N <96363667+snnandan123@users.noreply.github.com>
2022-02-16 23:09:02 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,17 @@
+package com.te.switchcondition;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		int n,i,fact=1;
+		System.out.println("Enter the number to find the factorial of a number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=1;i<=n;i++){
+			fact*=i;
+		}
+		System.out.println("Factorial of a given number "+n+" is "+fact);
+	}
+	
+}
---
+++ SwitchDays.java
@@ -0,0 +1,48 @@
+package com.te.switchcondition;
+
+import java.util.Scanner;
+
+public class SwitchDays {
+	public static void main(String[] args) {
+		String s;
+		
+		while(true){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("\nEnter the shortcut to get corresponding day");
+			System.out.println("Enter exit to terminate the program");
+			System.out.println("--------------------------------------------");
+			s=sc.nextLine();
+			if(s.equalsIgnoreCase("exit")){
+				break;
+			}
+			switch (s) {
+			case "sun":
+				System.out.println("Sunday");
+				break;
+			case "mon":
+				System.out.println("Moday");
+				break;
+			case "tue":
+				System.out.println("Tuesday");
+				break;
+			case "wed":
+				System.out.println("Wednesday");
+				break;
+			case "thu":
+				System.out.println("Thursday");
+				break;
+			case "fri":
+				System.out.println("Friday");
+				break;
+			case "sat":
+				System.out.println("Saturday");
+				break;
+			default:
+				System.out.println("Invalid choice");
+				break;
+			}
+
+		}
+		
+			}
+}
